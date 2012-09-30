ffa7cdb599090102ebb6da81c04635b959768a25
aumgn <aumgn@free.fr>
2012-10-01 01:26:45 +0200

    Add packages filters (Zip, Jar, Tar, TarGz and TarBz2)

--- src/main/java/fr/ethilvan/launcher/updater/PackageFilter.java
+++ src/main/java/fr/ethilvan/launcher/updater/PackageFilter.java
@@ -1,6 +1,7 @@
 package fr.ethilvan.launcher.updater;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,8 +9,13 @@ import java.io.OutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.jar.JarArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.compressors.xz.XZCompressorInputStream;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -39,6 +45,97 @@ public enum PackageFilter {
         }
     },
 
+    Zip {
+        public void filter(TaskDialog dialog, InputStream input,
+                File targetPath) {
+            ZipArchiveInputStream zipIS = null;
+            try {
+                zipIS = new ZipArchiveInputStream(input);
+                uncompressArchive(dialog, zipIS, targetPath);
+            } catch (IOException exc) {
+                Logger.getLogger(PackageFilter.class.getName())
+                        .log(Level.SEVERE,
+                                "Unable to uncompress downloaded file", exc);
+            } finally {
+                IOUtils.closeQuietly(zipIS);
+            }
+        }
+    },
+
+    Jar {
+        public void filter(TaskDialog dialog, InputStream input,
+                File targetPath) {
+            JarArchiveInputStream jarIS = null;
+            try {
+                jarIS = new JarArchiveInputStream(input);
+                uncompressArchive(dialog, jarIS, targetPath);
+            } catch (IOException exc) {
+                Logger.getLogger(PackageFilter.class.getName())
+                        .log(Level.SEVERE,
+                                "Unable to uncompress downloaded file", exc);
+            } finally {
+                IOUtils.closeQuietly(jarIS);
+            }
+        }
+    },
+
+    Tar {
+        public void filter(TaskDialog dialog, InputStream input,
+                File targetPath) {
+            TarArchiveInputStream tarIS = null;
+            try {
+                tarIS = new TarArchiveInputStream(input);
+                uncompressArchive(dialog, tarIS, targetPath);
+            } catch (IOException exc) {
+                Logger.getLogger(PackageFilter.class.getName())
+                        .log(Level.SEVERE,
+                                "Unable to uncompress downloaded file", exc);
+            } finally {
+                IOUtils.closeQuietly(tarIS);
+            }
+        }
+    },
+
+    TarGz {
+        public void filter(TaskDialog dialog, InputStream input,
+                File targetPath) {
+            InputStream inputStream = null;
+            try {
+                inputStream = new GzipCompressorInputStream(input);
+                TarArchiveInputStream tarIS =
+                        new TarArchiveInputStream(inputStream);
+                inputStream = tarIS;
+                uncompressArchive(dialog, tarIS, targetPath);
+            } catch (IOException exc) {
+                Logger.getLogger(PackageFilter.class.getName())
+                        .log(Level.SEVERE,
+                                "Unable to uncompress downloaded file", exc);
+            } finally {
+                IOUtils.closeQuietly(inputStream);
+            }
+        }
+    },
+
+    TarBz2 {
+        public void filter(TaskDialog dialog, InputStream input,
+                File targetPath) {
+            InputStream inputStream = null;
+            try {
+                inputStream = new BZip2CompressorInputStream(input);
+                TarArchiveInputStream tarIS =
+                        new TarArchiveInputStream(inputStream);
+                inputStream = tarIS;
+                uncompressArchive(dialog, tarIS, targetPath);
+            } catch (IOException exc) {
+                Logger.getLogger(PackageFilter.class.getName())
+                        .log(Level.SEVERE,
+                                "Unable to uncompress downloaded file", exc);
+            } finally {
+                IOUtils.closeQuietly(inputStream);
+            }
+        }
+    },
+
     TarXz {
         public void filter(TaskDialog dialog, InputStream input,
                 File targetPath) {
@@ -48,23 +145,7 @@ public enum PackageFilter {
                 TarArchiveInputStream tarIS =
                         new TarArchiveInputStream(inputStream);
                 inputStream = tarIS;
-
-                TarArchiveEntry entry;
-                while ((entry = tarIS.getNextTarEntry()) != null) {
-                    if (entry.isDirectory()) {
-                        continue;
-                    }
-
-                    File targetFile = new File(targetPath, entry.getName());
-                    Logger.getLogger(PackageFilter.class.getName())
-                            .info("Uncompressing " + entry.getName());
-                    dialog.setStatus("Décompression de " + entry.getName(),
-                            null);
-                    FileUtils.forceMkdir(targetFile.getParentFile());
-                    OutputStream output = new FileOutputStream(targetFile);
-                    IOUtils.copy(tarIS, output);
-                    IOUtils.closeQuietly(output);
-                }
+                uncompressArchive(dialog, tarIS, targetPath);
             } catch (IOException exc) {
                 Logger.getLogger(PackageFilter.class.getName())
                         .log(Level.SEVERE,
@@ -74,6 +155,27 @@ public enum PackageFilter {
             }
         }
     };
+
+    private static void uncompressArchive(TaskDialog dialog,
+            ArchiveInputStream is, File targetPath)
+                    throws FileNotFoundException, IOException {
+        ArchiveEntry entry;
+        while ((entry = is.getNextEntry()) != null) {
+            if (entry.isDirectory()) {
+                continue;
+            }
+
+            File targetFile = new File(targetPath, entry.getName());
+            Logger.getLogger(PackageFilter.class.getName())
+                    .info("Uncompressing " + entry.getName());
+            dialog.setStatus("Décompression de " + entry.getName(),
+                    null);
+            FileUtils.forceMkdir(targetFile.getParentFile());
+            OutputStream output = new FileOutputStream(targetFile);
+            IOUtils.copy(is, output);
+            IOUtils.closeQuietly(output);
+        }
+    }
 
     abstract void filter(TaskDialog dialog, InputStream input, File targetDir);
 }
